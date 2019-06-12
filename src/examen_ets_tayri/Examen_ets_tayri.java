7ce614aec5b6c34cca0bbad218e159401776bb32
AlumGS <AlumGS@PCProfesor>
2019-06-12 17:31:23 +0200

    creacion del proyecto ya inicializado

---
+++ src/examen_ets_tayri/Examen_ets_tayri.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examen_ets_tayri;
+
+/**
+ *
+ * @author AlumGS
+ */
+public class Examen_ets_tayri {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+          // Todo en una sola clase
+        /* Resultados calculadoos
+        Cálculo del Área Cuadrado
+        Cálculo del¨Área rectangulo
+        Cálculo del áreacirculo
+        Cálculo del Área del Triangulo
+        */
+        int alturaCuadrado=0;
+        double baseCuadrado=0;
+        double areaCuadrado=Math.pow(alturaCuadrado,2);
+        double baseRectangulo=0;
+        int alturaRectangulo=0;
+        double areaRectangulo=alturaRectangulo*baseRectangulo;
+        int radioCirculo=0;
+        double Pi=3.14;
+        double areaCirculo=(Pi * Math.pow(radioCirculo,2));
+        int baseTriangulo=0;
+        int alturaTriangulo=0;
+        int areaTriangulo=((baseTriangulo*alturaTriangulo)/2);
+        System.out.println(areaCuadrado);
+        System.out.println(areaRectangulo);
+        System.out.println(areaCirculo);
+        System.out.println(areaTriangulo);  
+    }
+    
+    
+}
